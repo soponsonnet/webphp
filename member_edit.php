6d99f8d5e2ce05f41e9d9919854f1ab37c652f68
soponsonnet <sopon.sonnet@gmail.com>
2023-07-12 15:25:36 +0700

    update

--- member_edit.php
+++ member_edit.php
@@ -5,10 +5,10 @@ $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result)
 ?>
 <form action="member_update.php?id=<?php echo $row[id] ?>" method="post" enctype="multipart/form-data">
-  ชื่อ<input type="text" name="firstname"value="<?php echo $row[firstname]; ?>"/>
-  นามสกุล<input type="text" name="lastname"value="<?php echo $row[lastname]; ?>"/><br/>
-  อีเมล<input type="email" name="email"value="<?php echo $row[email]; ?>"/><br/>
-  เบอร์โทร<input type="text" name="phone"value="<?php echo $row[phone]; ?>"/><br/>
+  ชื่อ<input type="text" name="firstname" value="<?php echo $row[firstname]; ?>"/>
+  นามสกุล<input type="text" name="lastname" value="<?php echo $row[lastname]; ?>"/><br/>
+  อีเมล<input type="email" name="email" value="<?php echo $row[email]; ?>"/><br/>
+  เบอร์โทร<input type="text" name="phone" value="<?php echo $row[phone]; ?>"/><br/>
   ที่อยู่<textarea name="address"> <?php echo $row[address]; ?></textarea> <br/>
   แนบรูป<input type="file" name="photo"/><br/>
   <input type="submit" value="update"/>
--- product_edit.php
+++ product_edit.php
@@ -5,9 +5,9 @@ $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result)
 ?>
 <form action="product_update.php?id=<?php echo $row[id] ?>" method="post" enctype="multipart/form-data">
-    ชื่อสินค้า<input type="text" name="name"value="<?php echo $row[name]; ?>"/><br/>
-    ราคา<input type="text" name="price"value="<?php echo $row[price]; ?>"/><br/>
-    จำนวนในคลัง<input type="text" name="stock"value="<?php echo $row[stock]; ?>"/><br/>
+    ชื่อสินค้า<input type="text" name="name" value="<?php echo $row[name]; ?>"/><br/>
+    ราคา<input type="text" name="price" value="<?php echo $row[price]; ?>"/><br/>
+    จำนวนในคลัง<input type="text" name="stock" value="<?php echo $row[stock]; ?>"/><br/>
     รายละเอียด<textarea name="description"><?php echo $row[description]; ?></textarea> <br/>
   
   <input type="submit" value="update"/>
